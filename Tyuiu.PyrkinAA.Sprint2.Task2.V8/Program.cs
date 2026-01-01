90b7c5e51cfc2492f1fc459e53cc1af31ec63595
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-01 22:00:08 +0500

    1

---
+++ Tyuiu.PyrkinAA.Sprint2.Task2.V8/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.PyrkinAA.Sprint2.Task2.V8.Lib;
+namespace Tyuiu.PyrkinAA.Sprint2.Task2.V8
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            Console.WriteLine("Vvedi x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vvedi y:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            bool res = dataService.CheckDotInShadedArea(x, y);
+
+            if (res)
+            {
+                Console.WriteLine("Pobeda");
+            }
+            else
+            {
+                Console.WriteLine("Mimo");
+            }
+        }
+    }
+}
