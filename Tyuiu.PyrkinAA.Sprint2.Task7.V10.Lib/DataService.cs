32a93bbbe3ab59248667035c704e66097a25c05f
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-04 16:35:34 +0500

    4

--- Tyuiu.PyrkinAA.Sprint2.Task7.V10.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint2.Task7.V10.Lib/DataService.cs
@@ -12,10 +12,10 @@ namespace Tyuiu.PyrkinAA.Sprint2.Task7.V10.Lib
 
             bool aboveParabola = y >= parabolaY;
             bool belowLine1 = y <= lineY1;
-            bool aboveLine2 = y >= lineY2;
+            bool belowLine2 = y <= lineY2;  
             bool rightOfYAxis = x >= 0;
 
-            return aboveParabola && belowLine1 && aboveLine2 && rightOfYAxis;
+            return aboveParabola && belowLine1 && belowLine2 && rightOfYAxis;
         }
     }
 }
