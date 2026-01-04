d5ced36dee3f26725a07851260c5fb7a4b425656
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-04 14:56:00 +0500

    1

---
+++ Tyuiu.PyrkinAA.Sprint2.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,120 @@
+﻿using Tyuiu.PyrkinAA.Sprint2.Task6.V6.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tyuiu.PyrkinAA.Sprint2.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindCardNameAndValue1()
+        {
+            DataService ds = new DataService();
+            int suit = 1;     
+            int rank = 12;     
+            string wait = "Дама пик";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue2()
+        {
+            DataService ds = new DataService();
+            int suit = 3;      
+            int rank = 6;     
+            string wait = "Шестерка бубен";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue3()
+        {
+            DataService ds = new DataService();
+            int suit = 4;     
+            int rank = 14;     
+            string wait = "Туз червей";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue4()
+        {
+            DataService ds = new DataService();
+            int suit = 2;    
+            int rank = 11;     
+            string wait = "Валет треф";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue5()
+        {
+            DataService ds = new DataService();
+            int suit = 1;     
+            int rank = 10;     
+            string wait = "Десятка пик";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue6()
+        {
+            DataService ds = new DataService();
+            int suit = 0;     
+            int rank = 10;     
+            string wait = "Десятка неизвестной масти";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue7()
+        {
+
+            DataService ds = new DataService();
+            int suit = 2;     
+            int rank = 5;     
+            string wait = "Неизвестное достоинство треф";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue8()
+        {
+            DataService ds = new DataService();
+            int suit = 3;     
+            int rank = 13;     
+            string wait = "Король бубен";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue9()
+        {
+            DataService ds = new DataService();
+            int suit = 4;     
+            int rank = 7;      
+            string wait = "Семерка червей";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidFindCardNameAndValue10()
+        {
+            DataService ds = new DataService();
+            int suit = 2;     
+            int rank = 9;      
+            string wait = "Девятка треф";
+            string res = ds.FindCardNameAndValue(suit, rank);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PyrkinAA.Sprint2.Task6.V6/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PyrkinAA.Sprint2.Task6.V6.Lib;
+namespace Tyuiu.PyrkinAA.Sprint2.Task6.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("Примеры работы программы:");
+            Console.WriteLine($"1. m=1, k=12: {ds.FindCardNameAndValue(1, 12)}");
+            Console.WriteLine($"2. m=3, k=6: {ds.FindCardNameAndValue(3, 6)}");
+            Console.WriteLine($"3. m=2, k=11: {ds.FindCardNameAndValue(2, 11)}");
+            Console.WriteLine($"4. m=4, k=14: {ds.FindCardNameAndValue(4, 14)}");
+            Console.WriteLine($"5. m=1, k=10: {ds.FindCardNameAndValue(1, 10)}");
+
+            Console.ReadKey();
+        }
+    }
+}
