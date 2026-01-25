eee05a50700d3e52644eb614789b80c3220706a4
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-25 23:07:57 +0500

    1

--- Tyuiu.PyrkinAA.Sprint2.Task4.V24.Test/DataServiceTest.cs
+++ Tyuiu.PyrkinAA.Sprint2.Task4.V24.Test/DataServiceTest.cs
@@ -1,74 +1,17 @@
 ﻿using Tyuiu.PyrkinAA.Sprint2.Task4.V24.Lib;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace Tyuiu.PyrkinAA.Sprint2.Task4.V24.Test
 {
     [TestClass]
-    public class DataServiceTest
+    public sealed class DataServiceTest
     {
         [TestMethod]
-        public void ValidCalculateConditionTrue() 
+        public void ValidCondition1()
         {
             DataService ds = new DataService();
-            double x = 30;
-            double y = 5;
+            double x = 1, y = 1;
             double res = ds.Calculate(x, y);
-            Assert.AreEqual(0, res, 0.001); 
-        }
-
-        [TestMethod]
-        public void ValidCalculateConditionFalse() 
-        {
-            DataService ds = new DataService();
-            double x = 5;
-            double y = 30;
-            double numerator = Math.Pow(y, 2) + Math.Pow(Math.Cos(x), 2) + 9;
-            double denominator = Math.Pow(x, 2) - Math.Pow(Math.Sin(y), 2) + 12;
-            double wait = Math.Round(numerator / denominator, 3);
-
-            double res = ds.Calculate(x, y);
-            Assert.AreEqual(wait, res, 0.001);
-        }
-
-        [TestMethod]
-        public void ValidCalculateEdgeCase() 
-        {
-            DataService ds = new DataService();
-            double x = 15;
-            double y = -7;
-            double numerator = Math.Pow(y, 2) + Math.Pow(Math.Cos(x), 2) + 9;
-            double denominator = Math.Pow(x, 2) - Math.Pow(Math.Sin(y), 2) + 12;
-            double wait = Math.Round(numerator / denominator, 3);
-
-            double res = ds.Calculate(x, y);
-            Assert.AreEqual(wait, res, 0.001);
-        }
-
-        [TestMethod]
-        public void ValidCalculateSimpleTrue()
-        {
-            DataService ds = new DataService();
-            double x = 100;
-            double y = 50;
-            double res = ds.Calculate(x, y);
-            Assert.AreEqual(0, res, 0.001);
-        }
-
-        [TestMethod]
-        public void ValidCalculateSimpleFalse()
-        {
-            DataService ds = new DataService();
-            double x = 2;
-            double y = 3;
-            double wait = Math.Round(
-                (Math.Pow(3, 2) + Math.Pow(Math.Cos(2), 2) + 9)
-                / (Math.Pow(2, 2) - Math.Pow(Math.Sin(3), 2) + 12),
-                3
-            );
-
-            double res = ds.Calculate(x, y);
-            Assert.AreEqual(wait, res, 0.001);
+           
         }
     }
 }
--- Tyuiu.PyrkinAA.Sprint2.Task4.V24/Program.cs
+++ Tyuiu.PyrkinAA.Sprint2.Task4.V24/Program.cs
@@ -1,30 +1,36 @@
 ﻿using Tyuiu.PyrkinAA.Sprint2.Task4.V24.Lib;
-namespace Tyuiu.PyrkinAA.Sprint2.Task4.V24
-{
-    class Program
-    {
-        static void Main (string[] args)
-        {
-            DataService ds = new DataService();
 
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
-            Console.WriteLine("***************************************************************************");
+DataService ds = new DataService();
+Console.Title = "Спринт 2 | Выполнил: Пыркин А. А. | АСОиУб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Тернарный оператор                                                *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #24                                                             *");
+Console.WriteLine("* Выполнил: Пыркин Александр Артемьевич | АСОиУб-25-1                     *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значение   *");
+Console.WriteLine("* переменных x,y с клавиатуры. Если x + 2 > y + 24, то                    *");
+Console.WriteLine("* z = x * ((y + 1)/(x + 2))^x, иначе (y^2 + cos(x)^2 + 9)/(x^2 - sin(y)^2 + 12)");
+Console.WriteLine("* Округлить полученное значение до трех знаков после запятой             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("*Введите значение переменой X: *");
-            double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение x:");
+double x = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("*Введите значение Y: *");
-            double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение y:");
+double y = Convert.ToDouble(Console.ReadLine());
 
-            double res = ds.Calculate(x, y);
+double res = ds.Calculate(x, y);
 
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ: *");
-            Console.WriteLine("***************************************************************************");
-            Console.WriteLine(" Значение функции = " + res);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
 
-            Console.ReadKey();
-        }
-    }
-}
+Console.WriteLine("Значение функции = " + res);
+
+Console.ReadKey();
