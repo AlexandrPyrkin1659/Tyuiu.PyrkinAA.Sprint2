4363a999eeffaa2cbbfdda40d638c0597b808749
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2026-01-25 23:12:42 +0500

    1

--- Tyuiu.PyrkinAA.Sprint2.Task4.V24.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint2.Task4.V24.Lib/DataService.cs
@@ -6,12 +6,22 @@ namespace Tyuiu.PyrkinAA.Sprint2.Task4.V24.Lib
     {
         public double Calculate(double x, double y)
         {
-            double result = (x + 2 > y + 24)
-                ? x * Math.Pow((y + 1) / (x + 2), x)
-                : (Math.Pow(y, 2) + Math.Pow(Math.Cos(x), 2) + 9) / (Math.Pow(x, 2) - Math.Pow(Math.Sin(y), 2) + 12);
+            double result;
 
-            
-            return double.Parse(result.ToString("0.000"));
+            if (x + 2 > y + 24)
+            {
+                result = x * Math.Pow((y + 1) / (x + 2), x);
+            }
+            else
+            {
+                
+                double numerator = Math.Pow(y, 2) + Math.Cos(Math.Pow(x, 2)) + 9; 
+                double denominator = Math.Pow(x, 2) - Math.Sin(Math.Pow(y, 2)) + 12; 
+                result = numerator / denominator;
+            }
+
+           
+            return Math.Truncate(result * 1000) / 1000;
         }
     }
 }
